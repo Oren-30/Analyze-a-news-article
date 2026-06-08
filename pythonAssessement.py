041d90f4ead3e65d654f480e57496dc46255cfa6
sirkiruthi <orencaroli@gmail.com>
2026-06-08 10:29:34 +0300

    Faet add changes to pythonAssessement.py

--- pythonAssessement.py
+++ pythonAssessement.py
@@ -3,3 +3,15 @@ import re
 def count_specific_word(text, word):
     words = re.findall(r'\b\w+\b', text.lower())
     return words.count(word.lower())
+
+def identify_most_common_word(text):
+    if not text.strip():
+        return None
+
+    words = re.findall(r'\b\w+\b', text.lower())
+    if not words:
+        return None
+
+    from collections import Counter
+    return Counter(words).most_common(1)[0][0]
+
